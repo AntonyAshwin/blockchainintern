2d16cc5d02ad6cb769acbbb8caeaa72bda3e15da
Antony Ashwin <30386360+AntonyAshwin@users.noreply.github.com>
2022-03-25 21:29:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -163,9 +163,10 @@ function App() {
             {grade}
           </p>
         </div>
-      </div>
 <p>Any queries regarding this decentralised application, hit me up on discord : Ahswin#6122</p>
  <p>Wanna dontate ? this is my Etherium/BAT wallet address : 0x27Ccc2bc5a7DCaF14380605C0a3962E67D8bBAFF</p>
+      </div>
+
     </div>
            
           
